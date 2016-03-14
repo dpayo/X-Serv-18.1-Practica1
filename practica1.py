7d5152ebd33c59c42a50a30410649dbb61c9f8af
Diego Payo Martinez <dpayomartinez@gmail.com>
2016-03-14 09:30:30 +0100

    Practica 1 v2.0

--- practica1.py
+++ practica1.py
@@ -15,7 +15,7 @@ import webapp
 import random
 import sys
 import re
-
+import csv
 
 class contentApp (webapp.webApp):
     """Simple web application for managing content.
@@ -36,11 +36,37 @@ class contentApp (webapp.webApp):
     dic1 = {}
     dic2 = {}
 
+
+    def toString(self,dic):
+        table=""
+        string=""
+        for key in dic:
+            table="<table style=width:50%><tr><td>URL</td> <td> KEY </td> </tr>"
+            string+="<tr><td>"+str(key.replace("%2F","/").replace("%3A",":"))+"</td>"+"<td>"+str(dic[key])+"</td></tr>";
+        return table+string;
+
+    def reader(self):
+        with open('datos.csv', 'rb') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in spamreader:
+                 self.dic1[row[0]]=row[1]
+                 self.dic2[int(row[1])]=row[0]
+                 aux=int(row[1])
+                 if(aux>self.seq):
+                     self.seq=aux
+
+    def writer(self):
+        with open('datos.csv', 'wb') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            for element in self.dic1:
+                spamwriter.writerow([element,self.dic1[element]])
+
     def parse(self, request):
         """Return the resource name (including /)"""
 
         metodo = request.split(' ', 2)[0]
         recurso = request.split(' ', 2)[1]
+
         if metodo == "PUT":
             cuerpo = request.split("\r\n\r\n", 1)[1]
         elif metodo == "POST":
@@ -56,41 +82,50 @@ class contentApp (webapp.webApp):
         Finds the HTML text corresponding to the resource name,
         ignoring requests for resources not in the dictionary.
         """
+
         (metodo, recurso, cuerpo) = resourceName
         if metodo == "GET":
-            if recurso in self.content.keys():
+            if recurso in self.content:
+                try:
+                    self.reader()
+                except IOError:
+                    self.dic1={}
                 httpCode = "200 OK"
-                htmlBody = "<html><body> <h1>Acortador URL's <br></h1> Ej: pepito.com " + self.content[recurso] + "</body></html>"
+                htmlBody = "<html><body bgcolor=#E6E6FA> <h1 style=color:blue >Acortador URL's <br></h1> Ej: pepito.com " + self.content[recurso] + self.toString(self.dic1)+"</body></html>"
+
             elif re.match("[0-9]", recurso[1]):
                 try:
                     num = int(recurso[1:])
                     try:
-                        httpCode = " 301 Moved Permanently\nLocation: http://" + self.dic2[num][13:]
+
+                        httpCode = " 302 Found\nLocation: http://" + self.dic2[num][13:]
                         htmlBody = ""
                     except KeyError:
                         httpCode = "404 Not Found"
-                        htmlBody = "<html><body> Recurso no disponible..." + self.form + "</body></html>"
+                        htmlBody = "<html><body bgcolor= #FA5858><strong> Recurso no disponible...</strong>" + self.form + "</body></html>"
                 except ValueError:
                         httpCode = "404 Not Found"
-                        htmlBody = "<html><body> Recurso no disponible..." + self.form + "</body></html>"
+                        htmlBody = "<html><body bgcolor=#FA5858 ><strong> Recurso no disponible...</strong>" + self.form + "</body></html>"
             else:
 
                     httpCode = "404 Not Found  "
-                    htmlBody = "<html><body>Recurso no disponible..." + self.form + "</body></html>"
+                    htmlBody = "<html><body bgcolor=#FA5858 ><strong>Recurso no disponible...</strong>" + self.form + "</body></html>"
         elif metodo == "POST":
             if cuerpo == "":
                 httpCode = "404"
-                htmlBody = "<html><body>Not Found"+self.form+" </body></html>"
+                htmlBody = "<html><body bgcolor=#FA5858 >Not Found"+self.form+" </body></html>"
             else:
                 if cuerpo in self.dic1:
                     httpCode = "200 OK"
-                    htmlBody = "<html><body>http://localhost:1234/" + str(self.dic1[cuerpo]) + "</body></html>"
+                    htmlBody = "<html><body bgcolor=#E6E6FA> URL: ---> <a href= http://localhost:1234/"+str(self.dic1[cuerpo])+"> http://localhost:1234/"+ str(self.dic1[cuerpo])+"</a></body></html>"
+
                 else:
                     self.seq = self.seq + 1
                     self.dic1[cuerpo] = self.seq
                     self.dic2[self.seq] = cuerpo
+                    self.writer();
                     httpCode = "200 OK"
-                    htmlBody = "<html><body> Url ---> http://localhost:1234/" + str(self.dic1[cuerpo]) + "</body></html>"
+                    htmlBody = "<html><body bgcolor=#E6E6FA> URL: ---> <a href= http://localhost:1234/"+str(self.dic1[cuerpo])+"> http://localhost:1234/"+ str(self.dic1[cuerpo])+"</a></body></html>"
         return (httpCode, htmlBody)
 
 
